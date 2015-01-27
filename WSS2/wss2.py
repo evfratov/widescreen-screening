a3702db3be2bb95200274d92b39dfd768718a9a2
evfratov <evfratov@gmail.com>
2015-01-28 02:17:33 +0300

    Bdate processing

--- WSS2/wss2.py
+++ WSS2/wss2.py
@@ -5,6 +5,7 @@ import argparse # парсинг коммандной строки
 import time # задержка
 import vk_api # https://github.com/python273/vk_api/
 import pandas # pandas для работы с данными
+import datetime # для работы с датами
 
 # Задержка между запросами в секундах
 SLEEP = 0.4
@@ -16,6 +17,8 @@ SEX = 'F'
 # минимальный и максимальный возраст
 MIN_AGE = 22
 MAX_AGE = 28
+# срок последней активности в днях
+LAST_SEEN = 50
 ## значения отношений
 # 1 – single
 # 2 – in a relationship
@@ -104,18 +107,36 @@ def primaryFiltering(allMembers):
 	# заполнение значений NaN в поле relation нулями для удобства
 	tempData.relation = tempData.relation.fillna(0)
 	
-	## нормализация полей city, country и last_seen
+	## нормализация полей city и country
 	# извлечение названия городов и стран, срока последнего посещения
 	tempData.city.loc[:][pandas.notnull(tempData.city.loc[:])] = tempData.city.loc[:][pandas.notnull(tempData.city.loc[:])].apply(lambda x: x['title'])
 	tempData.country.loc[:][pandas.notnull(tempData.country.loc[:])] = tempData.country.loc[:][pandas.notnull(tempData.country.loc[:])].apply(lambda x: x['title'])
-	tempData.last_seen = tempData.last_seen.apply(lambda x: x['time'])
-	tempData.last_seen = pandas.to_datetime(tempData['last_seen'], unit = 's')
 	# замещение NaN нулями городов и стран
 	tempData.city = tempData.city.fillna(0)
 	tempData.country = tempData.country.fillna(0)
 	
-	# здесь должна быть конвертация last_seen в нормальный формат дат,
-	# вычитание из неё сегодняшнего времени и отсечение всех с большим сроком неактивности
+	## фильтрация по last_seen и удаление более не нужной колонки
+	# извлечение из суб-словаря времени последнего посещения
+	tempData.last_seen = tempData.last_seen.apply(lambda x: x['time'])
+	# конвертация времени в формат даты
+	tempData.last_seen = pandas.to_datetime(tempData['last_seen'], unit = 's')
+	# вычисление последнего срока и фильтрация
+	tempData = tempData[tempData.last_seen.apply(lambda x: (datetime.datetime.today() - x).days) <= LAST_SEEN]
+	del tempData['last_seen']
+	
+	## пересчёт полных дат рождения в возраст и фильтрация	
+	# заполнение нулями NaN значений
+	tempData.bdate = tempData.bdate.fillna(0)
+	# написать функцию с полным процессингом дат без разделения датафреймов ??
+	# отбор полных возрастов в суб-датафрейм и конверсия в формат дат
+	tempDataFullbdates = tempData[tempData.bdate.apply(lambda x: len(str(x))) > 7]
+	tempDataFullbdates.bdates = tempDataFullbdates.bdates.convert_objects(convert_dates = 'coerce')
+	# вычисление возраста и фильтрация
+	tempDataFullbdates['age'] = tempDataFullbdates.bdate.apply(lambda x: round((datetime.datetime.today() - x).days/365, 1)))
+	tempDataFullbdates = tempDataFullbdates[tempDataFullbdates.age >= MIN_AGE]
+	tempDataFullbdates = tempDataFullbdates[tempDataFullbdates.age <= MAX_AGE]
+		
+	
 	
 	# здесь должно быть вычисление коэффициента трушности
 	
@@ -142,7 +163,7 @@ def main():
 	allMembers = work(vk)
 	# пред-обработка и конверсия списка в первичную таблицу кандидаток
 	primaryCandidatsTable = primaryFiltering(allMembers)
-	fl = open ('/home/evfr/primaryCandidats.csv', 'w')
+	fl = open ('/tmp/primaryCandidats.csv', 'w')
 	primaryCandidatsTable.to_csv(fl)
 
 # исполнение мастер-функции
